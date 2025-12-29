de1e29720a484bfc35b6b86b9ab61a6610257940
Luckius_ <herocreepyfr@gmail.com>
2025-12-29 15:49:09 +0400

    Add translation helper utility class

---
+++ src/main/java/io/github/mrlucky974/dracula_api/api/util/TranslationHelper.java
@@ -0,0 +1,24 @@
+package io.github.mrlucky974.dracula_api.api.util;
+
+import io.github.mrlucky974.dracula_api.DraculaAPI;
+import io.github.mrlucky974.dracula_api.api.ModEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricLanguageProvider;
+import net.minecraft.text.Text;
+import net.minecraft.text.TranslatableTextContent;
+import net.minecraft.util.Identifier;
+import org.jetbrains.annotations.NotNull;
+
+public class TranslationHelper {
+    public static void addText(@NotNull FabricLanguageProvider.TranslationBuilder translationBuilder, @NotNull Text text, @NotNull String value) {
+        if (text.getContent() instanceof TranslatableTextContent translatableTextContent) {
+            translationBuilder.add(translatableTextContent.getKey(), value);
+        } else {
+            ModEntrypoint.get(DraculaAPI.MOD_ID).logger()
+                    .warn("Failed to add translation for text: {}", text.getString());
+        }
+    }
+
+    public static String translateId(String prefix, Identifier id) {
+        return prefix + id.getNamespace() + "." + id.getPath().replace('/', '.');
+    }
+}
